a612bfb2002f6513c4356eedd36458478b10af01
JezBeard <jezbeard@gmail.com>
2023-09-08 07:20:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -85,6 +85,7 @@ def main():
         query = st.text_input("Ask question's about your PDF file:")
 
         suggestions = ["", "What is the main topic of the document?", "Can you summarize the document in 200 words?", "What are the key points mentioned in the document?", "What is the conclusion of the document?", "Can you provide an overview of the document's content?"]
+        add_vertical_space(2)
         suggestion = st.selectbox("Or select a suggestion:", suggestions, index=0)
     
 
