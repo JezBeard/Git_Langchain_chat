52d7b4ad4fd43b243db4066b6fa7bbe89a62c7ff
JezBeard <jezbeard@gmail.com>
2023-09-11 14:21:44 +0800

    New features

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-'''
-requirements.txt file contents:
- 
-langchain==0.0.154
-PyPDF2==3.0.1
-python-dotenv==1.0.0
-streamlit==1.18.1
-faiss-cpu==1.7.4
-streamlit-extras
-'''
 import streamlit as st
-#from dotenv import load_dotenv
 import pickle
+import requests
 from PyPDF2 import PdfReader
+from docx import Document
+from bs4 import BeautifulSoup
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -25,7 +17,7 @@ import openai
 import time
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
- 
+
 hide_streamlit_style = """
     <style>
     #MainMenu {visibility: hidden;}
@@ -33,76 +25,118 @@ hide_streamlit_style = """
     </style>
     """
 
+def read_pdf(file):
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
+def read_docx(file):
+    doc = Document(file)
+    text = ""
+    for paragraph in doc.paragraphs:
+        text += paragraph.text
+    return text
 
 def main():
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-    st.header("Chat to a PDF 💬👨🏻‍💻🤖")
+    st.header("Chat to a Document 💬👨🏻‍💻🤖")
     
-    # upload a PDF file
-    pdf = st.file_uploader("Upload your PDF (just one for now)", type='pdf')
- 
-    # st.write(pdf)
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
- 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=350,
-            chunk_overlap=50,
-            length_function=len
-            )
-        chunks = text_splitter.split_text(text=text)
- 
-        # # embeddings
-        store_name = pdf.name[:-4]
-        st.write(f'{store_name}')
-        # st.write(chunks)
- 
-        # if os.path.exists(f"{store_name}.pkl"):
-        #     with open(f"{store_name}.pkl", "rb") as f:
-        #         VectorStore = pickle.load(f)
-            # st.write('Embeddings Loaded from the Disk')s
-        #else:
-        embeddings = OpenAIEmbeddings()
-        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        with open(f"{store_name}.pkl", "wb") as f:
-            pickle.dump(VectorStore, f)
- 
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
- 
-        # Accept user questions/query
-        query = st.text_input("Ask question's about your PDF file:")
+    # Add a radio button for the user to select the input method
+    input_method = st.radio("Choose your input method:", ("Upload a document", "Paste text or web address"))
 
-        suggestions = ["", "What is the main topic of the document?", "Summarize the document in 200 words?", "Provide a bullet point list of the key points mentioned in the document?", "Create the headings and subheadings for Powerpoint slides", "Translate the first paragraph to French"]
-    
-        suggestion = st.selectbox("Or select a suggestion: (ENSURE QUESTION FIELD ABOVE IS BLANK)", suggestions, index=0)
+    text = ""
+    if input_method == "Upload a document":
+        # upload a PDF or Word file
+        file = st.file_uploader("Upload your PDF or Word document (just one for now)", type=['pdf', 'docx'])
 
-        if query:
-            # process query
-            docs = VectorStore.similarity_search(query=query, k=3)
-            llm = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], model_name='gpt-3.5-turbo', max_tokens=2000, temperature=0.5)
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb, st.spinner('Working on response...'):
-                time.sleep(3)
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
+        if file is not None:
+            if file.type == 'application/pdf':
+                text = read_pdf(file)
+            elif file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                text = read_docx(file)
+            else:
+                st.error("Unsupported file type. Please upload a PDF or Word document.")
+                return
+            store_name = file.name[:-4]
+        else:
+            st.error("Please upload a file.")
+            return
+    else:
+    # Paste text or URL
+        text_or_url = st.text_area("Paste your text or URL here: URLS must be in format https://")
+        store_name = "pasted_text_or_url"
 
-        elif suggestion:
-             # process suggestion
-            query = suggestion
-            docs = VectorStore.similarity_search(query=query, k=3)
-            llm = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], model_name='gpt-3.5-turbo', max_tokens=2000, temperature=0.5)
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb, st.spinner('Working on response...'):
-                time.sleep(3)
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
+        # Check if it's a URL
+        if text_or_url.startswith('http://') or text_or_url.startswith('https://'):
+            # It's a URL, fetch the content
+            response = requests.get(text_or_url)
+
+            # Check if it's a HTML page
+            if 'text/html' in response.headers['Content-Type']:
+                # Parse the HTML content with BeautifulSoup
+                soup = BeautifulSoup(response.content, 'html.parser')
+                # Extract all paragraph texts
+                text = ' '.join(p.get_text() for p in soup.find_all('p'))
+            else:
+                # It's not a HTML page, just use the raw content
+                text = response.text
+
+            store_name = "fetched_url_content"
+        else:
+            # It's not a URL, just use the pasted text
+            text = text_or_url
+
+        store_name = "fetched_url_content"
+
+    # Check if text is provided
+    if not text:
+        st.error("Please provide some text either by uploading a document or pasting text.")
+        return
+
+            # Process the pasted text
+            # The rest of your code remains the same...
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=350,
+        chunk_overlap=50,
+        length_function=len
+        )
+    chunks = text_splitter.split_text(text=text)
+
+    st.write(f'{store_name}')
+
+    embeddings = OpenAIEmbeddings()
+    VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+    with open(f"{store_name}.pkl", "wb") as f:
+        pickle.dump(VectorStore, f)
+
+    query = st.text_input("Ask question's about your document:")
+
+    suggestions = ["", "What is the main topic of the document?", "Summarize the document in 200 words?", "Provide a bullet point list of the key points mentioned in the document?", "Create the headings and subheadings for Powerpoint slides", "Translate the first paragraph to French"]
+
+    suggestion = st.selectbox("Or select a suggestion: (ENSURE QUESTION FIELD ABOVE IS BLANK)", suggestions, index=0)
+
+    if query:
+        docs = VectorStore.similarity_search(query=query, k=3)
+        llm = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], model_name='gpt-3.5-turbo', max_tokens=2000, temperature=0.5)
+        chain = load_qa_chain(llm=llm, chain_type="stuff")
+        with get_openai_callback() as cb, st.spinner('Working on response...'):
+            time.sleep(3)
+            response = chain.run(input_documents=docs, question=query)
+            print(cb)
+        st.write(response)
+
+    elif suggestion:
+        query = suggestion
+        docs = VectorStore.similarity_search(query=query, k=3)
+        llm = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], model_name='gpt-3.5-turbo', max_tokens=2000, temperature=0.5)
+        chain = load_qa_chain(llm=llm, chain_type="stuff")
+        with get_openai_callback() as cb, st.spinner('Working on response...'):
+            time.sleep(3)
+            response = chain.run(input_documents=docs, question=query)
+            print(cb)
+        st.write(response)
 
 if __name__ == '__main__':
     main()
