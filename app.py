13e5bd3c1f84db96bd7caa16b475f3a0dcbb70b1
JezBeard <jezbeard@gmail.com>
2023-09-11 14:29:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def read_docx(file):
     return text
 
 def main():
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    # st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
     st.header("Chat to a Document 💬👨🏻‍💻🤖")
     
     # Add a radio button for the user to select the input method
