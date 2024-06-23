7158e2db6b39d3edb62e7f3e848d33117df11f69
JezBeard <jezbeard@gmail.com>
2024-06-23 09:49:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
-#from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chains import RetrievalQA
 from langchain.memory import ConversationBufferMemory
@@ -20,6 +19,7 @@ from langchain_core.runnables import RunnablePassthrough
 import os
 import openai
 import time
+import pandas as pd
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
@@ -44,6 +44,10 @@ def read_docx(file):
         text += paragraph.text
     return text
 
+def read_csv(file):
+    df = pd.read_csv(file)
+    return df.to_string()
+
 def main():
     if 'text' not in st.session_state:
         st.session_state['text'] = ""
@@ -51,71 +55,70 @@ def main():
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
     st.header("Chat to a Document 💬👨🏻‍💻🤖")
     
-    # Add a radio button for the user to select the input method
     input_method = st.radio("Choose your input method:", ("Upload a document", "Paste text or web address"))
 
     if input_method == "Upload a document":
-        # upload a PDF or Word file
-        file = st.file_uploader("Load your PDF or Word document (just one for now)", type=['pdf', 'docx'])
+        file = st.file_uploader("Load your PDF, Word, or CSV document (just one for now)", type=['pdf', 'docx', 'csv'])
 
         if file is not None:
             if file.type == 'application/pdf':
                 st.session_state['text'] = read_pdf(file)
             elif file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
                 st.session_state['text'] = read_docx(file)
+            elif file.type == 'text/csv':
+                st.session_state['text'] = read_csv(file)
             else:
-                st.error("Unsupported file type. Please upload a PDF or Word document.")
+                st.error("Unsupported file type. Please upload a PDF, Word, or CSV document.")
                 return
         else:
             st.error("Please upload a file.")
             return
     else:
-        # Paste text or URL
         text_or_url = st.text_area("Paste your text or URL here: URLS must be in format https://")
         process_button = st.button("Process Text")
         
         if process_button:
             if text_or_url:
-                # Check if it's a URL
                 if text_or_url.startswith('http://') or text_or_url.startswith('https://'):
-                    # It's a URL, fetch the content
                     response = requests.get(text_or_url)
-
-                    # Check if it's a HTML page
                     if 'text/html' in response.headers['Content-Type']:
-                        # Parse the HTML content with BeautifulSoup
                         soup = BeautifulSoup(response.content, 'html.parser')
-                        # Extract all paragraph texts
                         text = ' '.join(p.get_text() for p in soup.find_all('p'))
-                        st.text_area("**Fetched Information from site:  Note that some websites block content access so the fetched information may be limited**", text)  # Display the fetched information in a text box
+                        st.text_area("**Fetched Information from site:  Note that some websites block content access so the fetched information may be limited**", text)
                     else:
-                        # It's not a HTML page, just use the raw content
                         text = response.text
-                        st.text_area("Fetched Information:", text)  # Display the fetched information in a text box
+                        st.text_area("Fetched Information:", text)
                 else:
-                    # It's not a URL, just use the pasted text
                     text = text_or_url
-                st.session_state['text'] = text  # Store the text in the session state
+                st.session_state['text'] = text
 
-    # Check if text is provided
-    if not st.session_state['text']:  # Use the text from the session state
+    if not st.session_state['text']:
         st.error("Please provide some text either by uploading a document or pasting text.")
         return
 
-    # Process the pasted text
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = text_splitter.split_text(text=st.session_state['text'])  # Use the text from the session state
+    chunks = text_splitter.split_text(text=st.session_state['text'])
 
     embeddings = OpenAIEmbeddings()
     VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
 
     query = st.text_input("Ask question's about your document:")
 
-    suggestions = ["", "What is the main topic of the document?", "Summarize the document in 200 words?", "Provide a bullet point list of the key points mentioned in the document?", "Create the headings and subheadings for Powerpoint slides", "Translate the first paragraph to French"]
+    suggestions = [
+        "",
+        "What is the main topic of the document?",
+        "Summarize the document in 200 words?",
+        "Provide a bullet point list of the key points mentioned in the document?",
+        "Create the headings and subheadings for Powerpoint slides",
+        "Translate the first paragraph to French",
+        "What are the column names in the CSV?",
+        "What is the average value in the [COLUMN_NAME] column?",
+        "How many rows are in the CSV file?"
+    ]
 
     suggestion = st.selectbox("Or select a suggestion: (ENSURE QUESTION FIELD ABOVE IS BLANK)", suggestions, index=0)
 
@@ -127,7 +130,9 @@ def main():
     """
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
 
-    if query:
+    if query or suggestion:
+        if suggestion:
+            query = suggestion
         docs = VectorStore.similarity_search(query=query, k=3)
         llm = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], model_name='gpt-4o', max_tokens=4000, temperature=0.2)
         
@@ -154,36 +159,6 @@ def main():
             response = chain.run(query)
             print(cb)
         st.write(response)
-
-    elif suggestion:
-        query = suggestion
-        docs = VectorStore.similarity_search(query=query, k=3)
-        llm = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], model_name='gpt-4o', max_tokens=4000, temperature=0.1)
-        
-        message = """
-        Answer this question using the provided context only. 
-
-        {question}
-
-        Context:
-        {context}
-        """
-
-        prompt = ChatPromptTemplate.from_messages([("human", message)])
-
-        chain = RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type="stuff",
-            retriever=VectorStore.as_retriever(),
-            chain_type_kwargs={"prompt": prompt},
-        )
-        
-        with get_openai_callback() as cb, st.spinner('Working on response...'):
-            time.sleep(3)
-            response = chain.run(query)
-            print(cb)
-        st.write(response)
-
 
 if __name__ == '__main__':
     main()
